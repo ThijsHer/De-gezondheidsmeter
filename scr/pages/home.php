d56672522268d4a9320c3399adb65666b0abbf89
ThijsHer <96177178+ThijsHer@users.noreply.github.com>
2023-11-15 09:01:13 +0100

    Added security

---
+++ scr/pages/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="../../Assets/CSS/header.css">
+    <link rel="stylesheet" href="../../Assets/CSS/main.css">
+    <link rel="stylesheet" href="../../Assets/CSS/home.css">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>De gezondheidsmeter</title>
+
+</head>
+<body>
+<?php
+include "../../scr/includes/header.php";
+?>
+<div class="buttons">
+    <button class="button">Dagschema invullen</button>
+    <button class="button">Afgelopen dagen</button>
+    <button class="button">Start</button>
+</div>
+</body>
+</html>
