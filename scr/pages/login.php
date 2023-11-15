2c13c15c0bd7d44ef7040a4e8fb009e284570636
ThijsHer <96177178+ThijsHer@users.noreply.github.com>
2023-11-15 13:33:11 +0100

    fixed login/register hash

--- scr/pages/login.php
+++ scr/pages/login.php
@@ -1,9 +1,42 @@
+<?php
+session_start();
+include "../includes/conn.php";
+
+$error_message = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+    $sql = "SELECT id, username, password FROM users WHERE username = ?";
+    $statement = $conn->prepare($sql);
+    $statement->bind_param('s', $username);
+    $statement->execute();
+    $statement->bind_result($id, $fetchedUsername, $hashedPassword);
+    $statement->fetch();
+
+    if ($fetchedUsername === $username) {
+        if (password_verify($password, $hashedPassword)) {
+            $_SESSION['user_id'] = $id;
+            $_SESSION['username'] = $fetchedUsername;
+            header("Location: index.php");
+            exit();
+        } else {
+            $error_message = "Invalid password.";
+        }
+    } else {
+        $error_message = "Invalid username.";
+    }
+
+    $statement->close();
+}
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
     <link rel="stylesheet" href="../../Assets/CSS/login.css">
@@ -13,15 +46,19 @@
 
 <div class="login">
     <div class="form">
-        <h1> Login </h1>
+        <h1>Login</h1>
+        <?php
+        if ($error_message !== '') {
+            echo '<p style="color: red;">' . $error_message . '</p>';
+        }
+        ?>
         <form action="" method="post">
-            <label>username</label>
+            <label>Username</label>
             <input name="username" type="text" class="logininput">
-            <label>password</label>
-            <input name="password" type="password" class="logininput">
-
+            <label>Password</label>
+            <input name="pwd" type="password" class="logininput">
             <button type="submit" name="submit" class="button">Login</button>
-            <button class="button">Sign up</button>
+            <button class="button"> <a href="login.php">Sign up</a></button>
         </form>
     </div>
 </div>
---
+++ scr/pages/register.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+include "../includes/conn.php";
+
+$error_message = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Registration logic
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+    $check_username_sql = "SELECT id FROM users WHERE username = ?";
+    $check_statement = $conn->prepare($check_username_sql);
+    $check_statement->bind_param('s', $username);
+    $check_statement->execute();
+    $check_statement->store_result();
+
+    if ($check_statement->num_rows > 0) {
+        $error_message = "Username already taken. Please choose another.";
+    } else {
+        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $insert_sql = "INSERT INTO users (username, password, admin, blocked) VALUES (?, ?, 0, 0)";
+        $insert_statement = $conn->prepare($insert_sql);
+        $insert_statement->bind_param('ss', $username, $hashed_password);
+
+        if ($insert_statement->execute()) {
+            $_SESSION['user_id'] = $insert_statement->insert_id;
+            $_SESSION['username'] = $username;
+            header("Location: index.php");
+            exit();
+        } else {
+            $error_message = "Error (´。＿。｀)";
+        }
+    }
+
+    $check_statement->close();
+    $insert_statement->close();
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+    <link rel="stylesheet" href="../../Assets/CSS/login.css">
+    <link rel="stylesheet" href="../../Assets/CSS/main.css">
+</head>
+<body>
+
+<div class="login">
+    <div class="form">
+        <h1>Register</h1>
+        <?php
+        if ($error_message !== '') {
+            echo '<p style="color: red;">' . $error_message . '</p>';
+        }
+        ?>
+        <form action="" method="post">
+            <label>Username</label>
+            <input name="username" type="text" class="logininput">
+            <label>Password</label>
+            <input name="pwd" type="password" class="logininput">
+            <button type="submit" name="submit" class="button">Register</button>
+            <button class="button"> <a href="login.php">Login</a></button>
+        </form>
+
+    </div>
+</div>
+
+</body>
+</html>
