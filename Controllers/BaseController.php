201ef73f5b1fd4d2cfb1ec104b7cd4d0d86a9d01
Joppe2003 <564285@edu.rocmn.nl>
2023-12-14 09:54:39 +0100

    check admin fix

--- Controllers/BaseController.php
+++ Controllers/BaseController.php
@@ -4,13 +4,13 @@ class BaseController
 {
     public function checkAdmin() {
         if(session_start() === true) {
-            if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 1) {
+            if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== "1") {
                 $this->redirect('login.php','../../scr/pages/');
             }
         } else {
             session_start();
 
-            if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== 1) {
+            if(!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== "1") {
                 $this->redirect('login.php','../scr/pages/');
             }
         }
