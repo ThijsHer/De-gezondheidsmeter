e3154b8558e8aadc0b8ef3cbcf291cde16624f16
dva-mano <557518@edu.rocmn.nl>
2024-01-18 11:03:22 +0100

    styling home + header

--- scr/includes/header.php
+++ scr/includes/header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="header-container">
         <div class="logo">
-            <a href="../pages/home.php"><h1>De Gezondsheidsmeter</h1></a>
+            <a href="../pages/home.php"><h1 class="logo-text">De Gezondsheidsmeter</h1></a>
         </div>
         <div class="buttons-header">
             <a href="../pages/home.php"><button class="header-button">Home</button></a>
